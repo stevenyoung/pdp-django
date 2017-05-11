24a3700382a2abbfce5859312840167c26c74e67
Steven Young <fivepockets@gmail.com>
2017-05-11 12:58:37 -0700

    use chrome canary for functional tests

--- pdp/functional_tests/tests.py
+++ pdp/functional_tests/tests.py
@@ -20,7 +20,7 @@ class HomePageVisitTestCase(unittest.TestCase):
     self.assertIn('places home', self.browser.title)
 
 
-class NewVisitorTestCase(LiveServerTestCase):
+class HomePageLiveServerTestCase(LiveServerTestCase):
 
   def setUp(self):
     chrome_options = webdriver.ChromeOptions()
@@ -43,5 +43,17 @@ class NewVisitorTestCase(LiveServerTestCase):
   def test_can_get_user_location_from_home_page(self):
     self.fail('finish this test')
 
-  def test_user_can_use_search_term_input_box(self):
-    self.fail('finish this test')
+
+class HomePageSearchTestCase(LiveServerTestCase):
+
+  def setUp(self):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = '/Applications/Google Chrome Canary.app/'
+    chrome_options.binary_location += 'Contents/MacOS/Google Chrome Canary'
+    self.browser = webdriver.Chrome(chrome_options=chrome_options)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_search_via_post(self):
+    self.fail('finish it!')
