1a960afcbc46e69c3400cd540589eed5f5887f20
Steven Young <fivepockets@gmail.com>
2017-05-11 13:00:49 -0700

    posted artwork and artist are saved with scene

--- pdp/places/views.py
+++ pdp/places/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.views import View
 
 from places.models import Scene
+from places.models import Artwork
+from places.models import Artist
 
 
 class HomePageView(View):
@@ -13,9 +15,14 @@ class HomePageView(View):
 
   def post(self, request):
     try:
+      title = request.POST['artwork']
+      posted_artist = request.POST['artist']
+      artist_ = Artist.objects.create(full_name=posted_artist)
+      artwork_ = Artwork.objects.create(title=title, artist=artist_)
       lng = request.POST['lng']
       lat = request.POST['lat']
       scene = Scene.objects.create(
+        artwork=artwork_,
         latitude=lat,
         longitude=lng,
         description=request.POST['description'])
