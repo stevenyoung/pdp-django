65124296bcbc2941d5eff8f07dea97b2407fcf6d
Steven Young <fivepockets@gmail.com>
2017-05-11 10:42:39 -0700

    using mysql backend for dev

--- pdp/pdp/settings.py
+++ pdp/pdp/settings.py
@@ -74,12 +74,32 @@ WSGI_APPLICATION = 'pdp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pdpdj',
+        'USER': 'pdpdj',
+        'PASSWORD': 'pdpdj',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'pdpdj',
+#         'USER': 'pdpdj',
+#         'PASSWORD': 'pdpdj',
+#     }
+# }
 
 
 # Password validation
