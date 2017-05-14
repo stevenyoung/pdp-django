4b8b2904d734c38f81b898b639ebab723ac5259b
Steven Young <fivepockets@gmail.com>
2017-05-14 12:15:39 -0700

    update path for scene view

--- pdp/places/urls.py
+++ pdp/places/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   url(r'^new$', views.HomePageView.as_view(), name='new_place'),
   url(r'^home$', views.HomePageView.as_view(), name='places_home'),
   url(r'^search/(?P<search_term>[-\w]+)', views.search_scenes, name='places_search'),
-  url(r'^(\d+)/$', views.view_scene, name='view_places'),
+  url(r'^(\d+)$', views.view_scene, name='view_places'),
 ]
