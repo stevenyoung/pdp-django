c7f82e2c45528877186651dbdda6b0657ff21d4c
Steven Young <fivepockets@gmail.com>
2017-05-15 13:07:40 -0700

    testing scene data api- returns 200, contains location data

--- pdp/places/tests/test_views.py
+++ pdp/places/tests/test_views.py
@@ -97,3 +97,26 @@ class SceneViewTest(TestCase):
   def test_scene_view_shows_artwork_title(self):
     response = self.client.get(self.new_scene_url)
     self.assertContains(response, 'Banned')
+
+
+class SceneAPITest(TestCase):
+  def setUp(self):
+    artist_ = Artist.objects.create(full_name="unknown")
+    artwork_ = Artwork.objects.create(artist=artist_, title='Banned in D.C.')
+    scene = Scene()
+    scene.description = 'can save a post request',
+    scene.longitude = -122.41575
+    scene.latitude = 37.749202
+    scene.artwork = artwork_
+    scene.save()
+    new_item = Scene.objects.first()
+    self.new_scene_url = '/places/api/' + str(new_item.id)
+
+  def test_requesting_scene_data_as_json_returns_status_200(self):
+    response = self.client.get(self.new_scene_url)
+    self.assertEqual(response.status_code, 200)
+
+  def test_location_data_available_in_api_response(self):
+    response = self.client.get(self.new_scene_url)
+    self.assertContains(response, '-122.41575')
+    self.assertContains(response, '37.749202')
