a085b27d76e70a0ef47f594037a76c448a022b40
Steven Young <fivepockets@gmail.com>
2017-05-15 13:09:03 -0700

    adding path for places api with scene id as parameter

--- pdp/places/urls.py
+++ pdp/places/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from places import views
 
 urlpatterns = [
+  url(r'^api/(?P<scene_id>[0-9])', views.get_scene_data, name='api_scene_data'),
   url(r'^new$', views.NewSceneView.as_view(), name='new_place'),
   url(r'^home$', views.HomePageView.as_view(), name='places_home'),
   url(r'^search/(?P<search_term>[-\w]+)', views.search_scenes, name='places_search'),
